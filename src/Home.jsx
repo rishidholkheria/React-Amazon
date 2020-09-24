d719e57e2a525ef25db42a12d9c96c471576a0a2
rishidholkheria <rishidholkheria2001@gmail.com>
2020-09-24 17:44:31 +0530

    Home page design

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,87 @@
-import React from 'react'
-import "./Home.css"
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
 
 function Home() {
-    return (
-        <div className="home">
-            <img className = "home_image"
-            src="https://wehaveahulk.co.uk/wp-content/uploads/2020/04/Hanna-S2.jpg" alt=""/>
+  return (
+    <div className="home">
+      <img
+        className="home_image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+        alt=""
+      />
+
+      <div className="home_items">
+        <div className="home_row">
+          <Product
+            id="12345678"
+            title="One Plus brand new Phone"
+            price={45000}
+            rating={5}
+            image="https://5.imimg.com/data5/WA/IN/MY-11183377/the-alchemist-500x500.png"
+          ></Product>
         </div>
-    )
+
+        <div className="home_row">
+          <Product
+            id="12345678"
+            title="One Plus brand new Phone"
+            price={45000}
+            rating={5}
+            image="https://5.imimg.com/data5/WA/IN/MY-11183377/the-alchemist-500x500.png"
+          ></Product>
+        </div>
+
+        <div className="home_row">
+          <Product
+            id="12345678"
+            title="One Plus brand new Phone"
+            price={45000}
+            rating={5}
+            image="https://5.imimg.com/data5/WA/IN/MY-11183377/the-alchemist-500x500.png"
+          ></Product>
+        </div>
+        <div className="home_row">
+          <Product
+            id="12345678"
+            title="One Plus brand new Phone"
+            price={45000}
+            rating={5}
+            image="https://5.imimg.com/data5/WA/IN/MY-11183377/the-alchemist-500x500.png"
+          ></Product>
+        </div>
+        <div className="home_row">
+          <Product
+            id="12345678"
+            title="One Plus brand new Phone"
+            price={45000}
+            rating={5}
+            image="https://5.imimg.com/data5/WA/IN/MY-11183377/the-alchemist-500x500.png"
+          ></Product>
+        </div>
+        <div className="home_row">
+          <Product
+            id="12345678"
+            title="One Plus brand new Phone"
+            price={45000}
+            rating={5}
+            image="https://5.imimg.com/data5/WA/IN/MY-11183377/the-alchemist-500x500.png"
+          ></Product>
+        </div>
+        <div className="home_row">
+          <Product
+            id="12345678"
+            title="One Plus brand new Phone"
+            price={45000}
+            rating={5}
+            image="https://5.imimg.com/data5/WA/IN/MY-11183377/the-alchemist-500x500.png"
+          ></Product>
+        </div>
+        
+
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
